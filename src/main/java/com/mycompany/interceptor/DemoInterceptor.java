004e590607d7b99cd95b37cafb307c28089944c7
xenahort <juan.carlos.wow.95@gmail.com>
2018-10-17 16:08:29 +0200

    Creada la estructura principal

---
+++ src/main/java/com/mycompany/interceptor/DemoInterceptor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.interceptor;
+
+/**
+ *
+ * @author Juan Carlos
+ */
+public class DemoInterceptor {
+
+    public static void main(String[] args) {
+        System.out.println("Ejecución de la clase principal");
+    }
+}
